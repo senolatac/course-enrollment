d2be32f5afe87b0a1a50527cfcde30e5243ee78f
senolatac <senol.atac.1@gmail.com>
2019-05-13 01:51:30 +0300

    Update GatewayApplication.java

--- zuul-gateway-service/src/main/java/com/sha/zuul/GatewayApplication.java
+++ zuul-gateway-service/src/main/java/com/sha/zuul/GatewayApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableEurekaClient
 @Import(WebConfig.class)
+@PropertySource("classpath:application.yml")
 public class GatewayApplication {
 
 	public static void main(String[] args) {
