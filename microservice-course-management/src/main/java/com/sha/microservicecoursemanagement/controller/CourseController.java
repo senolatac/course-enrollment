6120d78b2d891ae50b4ff684eedea75a06340bfc
Senol Atac <senol.atac.1@gmail.com>
2019-08-04 17:57:36 +0300

    initial

--- microservice-course-management/src/main/java/com/sha/microservicecoursemanagement/controller/CourseController.java
+++ microservice-course-management/src/main/java/com/sha/microservicecoursemanagement/controller/CourseController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.core.env.Environment;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -25,6 +27,25 @@ public class CourseController {
 
     @Autowired
     private CourseService courseService;
+	
+	@Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
+    private Environment env;
+
+    @Value("${spring.application.name}")
+    private String serviceId;
+
+    @GetMapping("/service/port")
+    public String getPort(){
+        return "Service is working at port : " + env.getProperty("local.server.port");
+    }
+
+    @GetMapping("/service/instances")
+    public ResponseEntity<?> getInstances() {
+        return ResponseEntity.ok(discoveryClient.getInstances(serviceId));
+    }
 
     @PostMapping("/user")
     public ResponseEntity<?> filterTransactions(@RequestBody Long userId){
